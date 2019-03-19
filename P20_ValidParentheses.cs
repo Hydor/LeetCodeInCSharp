36363e872c3d0389cc5b19374ff8c2ac1ad11144
Abu Hydor <bushiwo741@126.com>
2019-03-19 02:33:38 -0400

    Add files via upload
    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Note that an empty string is also considered valid.
    
    Example 1:
    
    Input: "()"
    Output: true

---
+++ P20_ValidParentheses.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace LeetCode
+{
+    static class P20_ValidParentheses
+    {
+       
+
+        public static bool IsValid(string s)
+        {
+            // Initialize a Dictionary and a stack to be used in the algorithm.
+            var dic = new Dictionary<char,char>();
+            dic.Add(')', '(');
+            dic.Add('}', '{');
+            dic.Add(']', '[');
+
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var num = (i >= s.Length) || (i < 0) ? "" : s.Substring(i, 1);
+
+                var c = Convert.ToChar(num);              
+
+                // If the current character is a closing bracket.
+                if (dic.ContainsKey(c))
+                {
+
+                    // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                    char topElement = stack.Count==0 ? '#' : stack.Pop();
+
+                    // If the mapping for this bracket doesn't match the stack's top element, return false.                  
+                    dic.TryGetValue(c, out char v);
+                    if (topElement != v )
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    // If it was an opening bracket, push to the stack.
+                    stack.Push(c);
+                }
+            }
+
+            // If the stack still contains elements, then it is an invalid expression.
+            return stack.Count==0;
+        }
+
+
+
+    }
+}
