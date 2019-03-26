1ca09c25bbc1dd0392d37f80a553636da654ab68
Abu Hydor <bushiwo741@126.com>
2019-03-26 00:17:07 -0400

    Add files via upload

---
+++ Array/P26_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array
+{
+    public class P26_RemoveDuplicatesFromSortedArray
+    {
+
+        // Solution: Two Pointer
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Count() == 0) return 0;
+            var pointer = 1;
+            var currentNumber = nums[0];
+            for (var i = 1; i < nums.Count(); i++)
+            {
+                if (nums[i] != currentNumber)
+                {
+                    nums[pointer] = nums[i];
+                    pointer++;
+                    currentNumber = nums[i];
+                }
+            }
+            return pointer;
+        }
+    }
+}
---
+++ Array/P27_RemoveElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    class P27_RemoveElement
+    {
+
+        //Approach 1: Two Pointers
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int pointer = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] != val) 
+                {
+                    nums[pointer] = nums[i];
+                    pointer++;
+                }
+                
+            }
+            return pointer;
+        }
+
+
+        //  Approach 2: Two Pointers - move the END element to the position
+        public int RemoveElement2(int[] nums, int val)
+        {
+            int n = nums.Count();
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[n - 1];
+                    n--;
+                    i--;
+                }
+            }
+            return n;
+        }
+
+    }
+}
