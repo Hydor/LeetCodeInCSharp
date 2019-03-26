c1f7bd5a5b4c4963f50780c70cfd8328ec5e2a46
Abu Hydor <bushiwo741@126.com>
2019-03-26 02:31:07 -0400

    Add files via upload

---
+++ Array/P277_FindCelebrity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    public class P277_FindCelebrity
+    {
+        public static int FindCelebrity(int n)
+        {
+            if (n<2) return -1;
+            for (int i = 0; i < n;)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (!Knows(j, i)) break;
+                    if (j == n - 1)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            if (Knows(i,k) && i!=k) break;
+                            if (k == n - 1) return i;
+                        }                        
+                    } 
+                }
+                i++;
+            }
+            return -1;
+        }
+
+        static  bool Knows(int i, int j)
+        { 
+            //example 1
+            if (i == 0 && j == 0) return true;
+            if (i == 1 && j == 0) return true;
+            if (i == 2 && j == 0) return false;
+            if (i == 0 && j == 1) return false;
+            if (i == 1 && j == 1) return true;
+            if (i == 2 && j == 1) return false;
+            if (i == 0 && j == 2) return true;
+            if (i == 1 && j == 2) return true;
+            if (i == 1 && j == 2) return true;
+
+            return true;
+        }
+    }
+
+}
---
+++ Array/P80_RemoveDuplicates2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P80_RemoveDuplicates2
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Count() == 0) return 0;
+            var pointer = 0;
+            var currentNumber = nums[0];
+            var curNumCount = 0;
+
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] == currentNumber && curNumCount >= 2)
+                {
+                    curNumCount++;
+
+                }
+                else if (nums[i] == currentNumber && curNumCount < 2)
+                {
+                    curNumCount++;
+                    nums[pointer] = nums[i];
+                    pointer++;
+                }
+                else
+                {
+                    curNumCount = 1;
+                    nums[pointer] = nums[i];
+                    pointer++;
+                }
+                currentNumber = nums[i];
+            }
+
+            return pointer;
+        }
+    }
+}
