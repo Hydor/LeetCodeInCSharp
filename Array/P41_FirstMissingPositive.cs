8c6759c509cae355f4637978010d6a7e380df626
Abu Hydor <bushiwo741@126.com>
2019-03-28 23:56:15 -0400

    Add files via upload

---
+++ Array/P41_FirstMissingPositive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P41_FirstMissingPositive
+    {
+
+        // Another Array   --  82.70%   &  76.36%
+        public int FirstMissingPositive(int[] nums)
+        {
+            var NewArr = new int[nums.Count()+2]; 
+            foreach (var i in nums)
+            {
+                if (i > 0 && i <= nums.Count()) { NewArr[i] = 1; }
+            }
+            for (var i = 1; i < NewArr.Count(); i++)
+            {
+                if (NewArr[i] < 1) return i;
+            }
+            return 0;
+
+        }
+    }
+}
