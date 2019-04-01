f2c338671f4d4cfe727847814126ce3f6b2b719d
Abu Hydor <bushiwo741@126.com>
2019-04-01 02:08:33 -0400

    Add files via upload

---
+++ Array/P274_HIndex.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P274_HIndex
+    {
+
+        //Sorting    88.61%  83.33%
+        public static int HIndex(int[] citations)
+        {
+            BaseClass.QuickSort.QuickSortArray(citations);
+            int i = 0;
+            while (i < citations.Count() && citations[citations.Count() - 1 - i] > i)
+            {
+                i++;
+            }
+            return i;
+        }
+
+        // System.Array.Sort    88.61%   16.67%
+        public static int HIndex2(int[] citations)
+        {
+            if (citations.Count()==0){return 0;}
+            System.Array.Sort(citations);
+            int i = 0;
+            while (i < citations.Count() && citations[citations.Count() - 1 - i] > i)
+            {
+                i++;
+            }
+            return i;
+        }
+
+
+    }
+}
---
+++ Array/P275_HIndex2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P275_HIndex2
+    {
+        public static int HIndex(int[] citations)
+        {
+            int i = 0;
+            while (i < citations.Count() && citations[citations.Count() - 1 - i] > i)
+            {
+                i++;
+            }
+            return i;
+        }
+    }
+}
