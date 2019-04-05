8dcafd3111cbef18c4b291c7eef6139717f50437
Abu Hydor <bushiwo741@126.com>
2019-04-04 23:18:34 -0400

    Add files via upload

---
+++ Array/P771_Jewels_Stones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P771_Jewels_Stones
+    {
+
+
+        // Dictionary    100%   14.6%
+        public static int NumJewelsInStones(string J, string S)
+        {
+            var count = 0;
+            var jewDict = new Dictionary<char, int>();
+            var jArr = J.ToCharArray();
+            var sArr = S.ToCharArray();
+            
+            for (var i = 0; i < jArr.Count(); i++)
+            {
+                jewDict.Add(jArr[i], 0);
+            }
+            foreach (var i in sArr)
+            {
+                if (jewDict.ContainsKey(i))
+                {
+                    var oldvalue = 0;
+                    jewDict.TryGetValue(i, out oldvalue);
+                    jewDict[i] = oldvalue + 1;
+                }               
+            }
+            for (var i = 0; i < jArr.Count(); i++)
+            {
+                count += jewDict[jArr[i]];
+            }
+            return count;
+        }
+    }
+}
