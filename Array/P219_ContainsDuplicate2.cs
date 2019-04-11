0169bfb39e1fe36b4147dfba03a150c49a37de20
Abu Hydor <bushiwo741@126.com>
2019-04-10 20:18:27 -0400

    Add files via upload

---
+++ Array/P219_ContainsDuplicate2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P219_ContainsDuplicate2
+    {
+        // Dictionary  O(n)  68.55%   31.25% 
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var i = 0;
+            var dic = new Dictionary<int, int>();
+            while (i < nums.Count())
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    if (i - dic[nums[i]] <= k) { return true; }
+                    else { dic[nums[i]] = i; }
+                }
+                else
+                {
+                    dic.Add(nums[i], i);
+                }
+                i++;
+            }
+            return false;
+        }
+    }
+}
---
+++ Array/P55_JumpGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P55_JumpGame
+    {
+        //Greedy    78.75%    97.30%
+        public bool CanJump(int[] nums)
+        {
+            int lastPos = nums.Count() - 1;
+            for (int i = nums.Count() - 1; i >= 0; i--)
+            {
+                if (i + nums[i] >= lastPos)
+                {
+                    lastPos = i;
+                }
+            }
+            return lastPos == 0;
+        }
+    }
+}
