d9c7d6a1b192b9d2fc5164c8e980b5c79eb0b4a7
Abu Hydor <bushiwo741@126.com>
2019-04-12 03:32:23 -0400

    Add files via upload

---
+++ Array/P11_ContainerWithMostWater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P11_ContainerWithMostWater
+    {
+        //Two Pointer       O(n)         92.90%    16.79%
+        public int MaxArea(int[] height)
+        {
+            var leftIndex = 0;
+            var rightIndex = height.Count() - 1;
+            var max = 0;
+            while (leftIndex != rightIndex)
+            {
+                max = Math.Max(max, (rightIndex - leftIndex) * Math.Min(height[leftIndex], height[rightIndex]));
+                if (height[leftIndex] > height[rightIndex]) rightIndex--;
+                else { leftIndex++; }
+            }
+            return max;
+        }
+    }
+}
