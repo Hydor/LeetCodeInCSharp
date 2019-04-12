1005027b1efcdc63b9ea1116a485167c9d16e6e9
Abu Hydor <bushiwo741@126.com>
2019-04-11 23:45:12 -0400

    Add files via upload

---
+++ Array/P45_JumpGame2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P45_JumpGame2
+    {
+        // Greedy    O(n)    76.11%    81.82%
+        public int Jump(int[] nums)
+        {
+            if (nums.Count() <= 1) return 0;
+            var steps = 0;
+            var stepEnd = 0;
+            var currentFurthest = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (stepEnd >= nums.Count() - 1) return steps;
+                currentFurthest = Math.Max(i + nums[i], currentFurthest);
+                if (i == stepEnd)
+                {
+                    stepEnd = currentFurthest;
+                    steps++;
+                }
+            }
+            return steps;
+        }
+    }
+}
