98ac09ba1356b692163e66c497ffa9c88d4f0a42
Abu Hydor <bushiwo741@126.com>
2019-04-12 02:06:18 -0400

    Add files via upload

---
+++ Array/P122_BestTimeBuyAndSell.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P122_BestTimeBuyAndSell
+    {
+        // One pass  O(n)    67.11%     80.39%
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Count() <= 1) return 0;
+            var profit = 0;
+            for (int i = 0; i < prices.Count() - 1; i++)
+            {
+                if (prices[i + 1] > prices[i]) profit += prices[i + 1] - prices[i];
+            }
+            return profit;
+        }
+    }
+}
