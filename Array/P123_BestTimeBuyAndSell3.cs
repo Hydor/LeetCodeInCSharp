b30e2355ca6e95043da0370db7249b71e8740149
Abu Hydor <bushiwo741@126.com>
2019-04-12 02:39:58 -0400

    Add files via upload

---
+++ Array/P123_BestTimeBuyAndSell3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Array
+{
+    class P123_BestTimeBuyAndSell3
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var buy1 = int.MinValue;
+            var sell1 = 0;
+            var buy2 = int.MinValue;
+            var sell2 = 0;
+            for (int i = 0; i < prices.Count(); i++)
+            {
+                buy1 = Math.Max(buy1, 0 - prices[i]);
+                sell1 = Math.Max(sell1, prices[i] + buy1);
+                buy2 = Math.Max(buy2, sell1 - prices[i]);
+                sell2 = Math.Max(sell2, prices[i] + buy2);
+            }
+            return sell2;
+        }
+    }
+}
