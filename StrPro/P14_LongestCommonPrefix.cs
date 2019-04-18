ee445c19ab7d7b46baaad26695bf6af5a3d5797d
Abu Hydor <bushiwo741@126.com>
2019-04-18 00:21:11 -0400

    Add files via upload

---
+++ StrPro/P14_LongestCommonPrefix.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+
+namespace LeetCode.StrPro
+{
+    class P14_LongestCommonPrefix
+    {
+
+
+        //96%   52%
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Count() == 0) return "";
+            var l = int.MaxValue;
+            foreach (var s in strs)
+            {
+                if (s.Length < l) l = s.Length;
+            }
+            var lcp = strs[0].Substring(0, l);
+
+            for (var i = 0; i < l; i++)
+            {
+                foreach (var s in strs)
+                {
+                    if (lcp[i] != s[i]) return lcp.Substring(0, i);
+                }
+            }
+            return lcp;
+        }
+    }
+}
