1e60aeadaf9ec158960ed3892978ccab8c3ac60a
Abu Hydor <bushiwo741@126.com>
2019-04-20 00:44:40 -0400

    Add files via upload

---
+++ StrPro/P344_ReverseString.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Linq;
+namespace LeetCode.StrPro
+{
+    class P344_ReverseString
+    {
+        //91.64%    64.67%
+        public void ReverseString(char[] s)
+        {
+            var temp = ' ';
+
+            for (int i = 0; i < s.Count() / 2; i++)
+            {
+                temp = s[i];
+                s[i] = s[s.Count() - 1 - i];
+                s[s.Count() - 1 - i] = temp;
+            }
+        }
+    }
+}
