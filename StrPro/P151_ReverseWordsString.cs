f3f1c74d189c43a720c3e27f2e94e98604c1bb09
Abu Hydor <bushiwo741@126.com>
2019-04-20 01:10:17 -0400

    Add files via upload

---
+++ StrPro/P151_ReverseWordsString.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode.StrPro
+{
+    class P151_ReverseWordsString
+    {
+        public static string ReverseWords(string s)
+        {
+            while (s.Contains("  ")) { s = s.Replace("  ", " "); }
+            var sArr = s.Split(' ');
+            var sb = new StringBuilder();
+            for (var i = sArr.Count() - 1; i >= 0; i--)
+            {
+                if (sArr[i].ToString()!="")
+                {
+                    sb.Append(sArr[i]);
+                    if (i != 0 && sArr[i-1] != "" ) sb.Append(" ");
+                }
+            }            
+            return sb.ToString();
+        }
+    }
+}
