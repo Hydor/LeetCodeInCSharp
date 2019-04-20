14259fff1437be59d9eb1f9f3bfdab60043ac620
Abu Hydor <bushiwo741@126.com>
2019-04-19 23:45:05 -0400

    Add files via upload

---
+++ StrPro/P387_FirstUniqueChar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.StrPro
+{
+    class P387_FirstUniqueChar
+    {
+        // Dictionary     64.99%      30.30%
+        public int FirstUniqChar(string s)
+        {
+            var dic = new Dictionary<char, int>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i])) dic[s[i]] = -1;
+                else dic.Add(s[i], i);
+            }
+            foreach (var i in dic)
+            {
+                if (i.Value >= 0) return i.Value;
+            }
+            return -1;
+        }
+
+     
+
+
+    }
+}
