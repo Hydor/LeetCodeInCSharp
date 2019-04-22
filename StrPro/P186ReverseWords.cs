f338263c87a398ddcb69ca57aa733f68ffd99b04
Abu Hydor <bushiwo741@126.com>
2019-04-22 01:42:41 -0400

    Update P186ReverseWords.cs

--- StrPro/P186ReverseWords.cs
+++ StrPro/P186ReverseWords.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.StrPro
 {
-    class P189
+    class P186ReverseWords
     {
+        public static void ReverseWords(char[] str)
+        {
+            System.Array.Reverse(str);
+            var beginIndex = 0;
+            var endIndex = 0;
+            var temp = ' ';
+            for (int i = 0; i < str.Count(); i++)
+            {
+                if (str[i] == ' ')
+                {
+                    endIndex = i - 1;
+                    for (int j = 0; j <= (endIndex - beginIndex) / 2; j++)
+                    {
+                        temp = str[j + beginIndex];
+                        str[j + beginIndex] = str[endIndex - j];
+                        str[endIndex - j] = temp;
+                    }
+                    beginIndex = i + 1;
+                }
+                else if (i == str.Count() - 1)
+                {
+                    endIndex = i;
+                    for (int j = 0; j <= (endIndex - beginIndex) / 2; j++)
+                    {
+                        temp = str[j + beginIndex];
+                        str[j + beginIndex] = str[endIndex - j];
+                        str[endIndex - j] = temp;
+                    }
+                    beginIndex = i + 1;
+                }
+            }
+        }
     }
 }
