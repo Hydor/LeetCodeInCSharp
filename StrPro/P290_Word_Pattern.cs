ee5592cf41fc0c300e1c424c6b8cd3dc41677c64
Abu Hydor <bushiwo741@126.com>
2019-04-23 18:54:22 -0400

    Add files via upload

---
+++ StrPro/P290_Word_Pattern.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LeetCode.StrPro
+{
+    class P290_Word_Pattern
+    {
+        public bool WordPattern(string pattern, string str)
+        {
+
+            var t = str.Split(' ');
+            if (pattern.Length != t.Count()) return false;
+            var dict = new Dictionary<char, string>();
+
+            var dictT = new Dictionary<string, char>();
+            for (var i = 0; i < pattern.Length; i++)
+            {
+                if (dict.ContainsKey(pattern[i]))
+                {
+                    if (dict[pattern[i]] != t[i]) return false;
+                }
+                else { dict.Add(pattern[i], t[i]); }
+                if (dictT.ContainsKey(t[i]))
+                {
+                    if (dictT[t[i]] != pattern[i]) return false;
+                }
+                else { dictT.Add(t[i], pattern[i]); }
+            }
+            return true;
+        }
+    }
+}
