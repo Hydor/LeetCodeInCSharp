6dac5f51393b856c1fbd1a2c9298565469aadc7b
Abu Hydor <bushiwo741@126.com>
2019-04-24 20:04:33 -0400

    Add files via upload

---
+++ MathPro/P7_ReverseInt.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace LeetCode.MathPro
+{
+    class P7_ReverseInt
+    {
+        //100%   98%
+        public static int Reverse(int x)
+        {
+                long result = 0l;
+                while (x != 0)
+                {
+                    result = result * 10 + x % 10;
+                    x = x / 10;
+                }
+                int test = (int)result;
+                return (test == result) ? test : 0;
+            }
+        
+    }
+}
