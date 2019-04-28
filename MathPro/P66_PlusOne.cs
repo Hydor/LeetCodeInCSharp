ae78ba6cbe132099872eedcf7b4446c2adb16675
Abu Hydor <bushiwo741@126.com>
2019-04-27 23:39:06 -0400

    Add files via upload

---
+++ MathPro/P66_PlusOne.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Linq;
+
+namespace LeetCode.MathPro
+{
+    class P66_PlusOne
+    {
+        //      99.93%          73.97%
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Count() - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+                else
+                {
+                    digits[i] = 0;
+                    if (i == 0)
+                    {
+                        var newdigits = new int[digits.Count() + 1];
+                        newdigits[0] = 1;
+                        return newdigits;
+                    }
+                }
+            }
+
+            return digits;
+        }
+    }
+}
