73f1e4f39ed5a8834800ecbb64519d6b9ab3fd10
Abu Hydor <bushiwo741@126.com>
2019-05-13 20:41:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Array;
+using System;
+namespace LeetCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //var number1 = Convert.ToInt32(Console.ReadLine());
+                //var number2 = Convert.ToInt32(Console.ReadLine());
+
+                var a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54 };
+                var strarr = new string[] { "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do" };
+                
+                Console.WriteLine(MathPro.P69_Divide2Integers.Divide2(-2147483648, 3));
+                var str = Console.ReadLine();
+                var str2 = Console.ReadLine();
+            }
+        }
+
+
+
+
+        
+
+
+    }
+}
