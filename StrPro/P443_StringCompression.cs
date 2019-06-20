bd3c4679334ccb7c5ee6e6072c1362b01c3cfacf
Abu Hydor <bushiwo741@126.com>
2019-06-19 23:14:45 -0400

    p 443

---
+++ StrPro/P443_StringCompression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.StrPro
+{
+    class P443_StringCompression
+    {
+        public int Compress(char[] chars)
+        {
+            int len = 0;
+            for (int i = 0, j = 0; i < chars.Count(); i = j)
+            {
+                while (j < chars.Count() && chars[i] == chars[j]) j++;
+                chars[len++] = chars[i];
+                if ((j - i) <= 1) continue;
+                foreach (char count in (j - i).ToString())
+                {
+                    chars[len++] = count;
+                }
+            }
+            return len;
+        }
+    }
+}
