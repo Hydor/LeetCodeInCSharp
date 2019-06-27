2bb86e165314b5506ee72cf918889c0be74e5ee2
Abu Hydor <bushiwo741@126.com>
2019-06-26 22:22:14 -0400

    81

---
+++ BinarySearch/P153_FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BinarySearch
+{
+    class P153_FindMinimuminRotatedSortedArray
+    {
+        //Binary Search : note the edge situation   like    61234    34561   
+        public int FindMin(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            var start = 1;
+            var end = nums.Length - 2;
+            if (nums.Length == 1 || nums[0] < nums[end + 1])
+            {
+                return nums[0];
+            }
+            if (nums[start] < nums[end] && nums[start] > nums[0])
+            {
+                return nums[end + 1];
+            }
+
+            var mid = 0;
+            while (start + 1 < end)
+            {
+                mid = start + (end - start) / 2;
+                if (nums[mid] > nums[mid + 1] && nums[mid] < nums[mid - 1])
+                {
+                    return nums[mid];
+                }
+
+                else if (nums[mid] > nums[start] && nums[mid] > nums[0])
+                {
+                    start = mid;
+                }
+                else
+                {
+                    end = mid;
+                }
+
+            }
+            if (nums[start] > nums[0] && nums[end] > nums[0]) return nums[nums.Length - 1];
+            //Console.WriteLine(start);
+            // Console.WriteLine(end);
+            return nums[start] > nums[end] ? nums[end] : nums[start];
+        }
+    }
+}
