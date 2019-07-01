0a928e84dd8dadf68d488590628bc51c539deefa
Abu Hydor <bushiwo741@126.com>
2019-07-01 13:00:27 -0400

    trees

---
+++ TreePro/P100_SameTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TreePro
+{
+
+    class P100_SameTree
+    {
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+
+        // Recursive 
+        // Bug 1: isSame should be GLOBAL variable, cannot passed as parameters, 
+        // Bug 2: p & q both null or one of them, should be aware
+        public bool isSame;
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            if (p == null || q == null)
+            {
+                return false;
+            }
+            isSame = true;
+            TraverseHelper(p, q);
+            return isSame;
+        }
+        void TraverseHelper(TreeNode p, TreeNode q)
+        {
+            if (!isSame || (q == null && p == null))
+            {
+                return;
+            }
+
+            if (q == null || p == null)
+            {
+                isSame = false;
+                return;
+            }
+            if (p.val != q.val)
+            {
+                isSame = false;
+
+                return;
+            }
+            TraverseHelper(p.left, q.left);
+            TraverseHelper(p.right, q.right);
+
+        }
+    }
+}
--- TreePro/P145_BinaryTreePostorderTraversal.cs
+++ TreePro/P145_BinaryTreePostorderTraversal.cs
@@ -37,5 +37,30 @@ namespace LeetCode.TreePro
             PostorderTraversalHelper(root.right, list);
             list.Add(root.val);
         }
+
+        //Approach 2: Stack
+        public IList<int> Postorder(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root == null) return result;
+            var stack = new Stack<TreeNode>();
+            var curr = root;
+            stack.Push(curr);
+            while (stack.Any())
+            {
+                curr = stack.Pop();   
+                result.Insert(0, curr.val);     // Insert to first index. Cause DFS is Top-> Bottom, but Postorder is Bottom -> Top
+                if (curr.left != null)
+                {
+                    stack.Push(curr.left);
+                }
+                if (curr.right != null)
+                {
+                    stack.Push(curr.right);
+                }
+            }
+
+            return result;
+        }
     }
 }
