ee9c336f867bc4ec0ec3693d2ffc7a8b8e786624
Abu Hydor <bushiwo741@126.com>
2019-07-01 01:42:03 -0400

    new

---
+++ StrPro/P819_MostCommonWord.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.StrPro
+{
+    class P819_MostCommonWord
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            if (paragraph.Length == 0)
+            {
+                return "";
+            }
+            var dict = new Dictionary<string, int>();
+            var currWord = "";
+            var lowchar = new char();
+            for (var i = 0; i < paragraph.Count(); i++)
+            {
+                lowchar = char.ToLower(paragraph[i]);
+                if (!IsLetter(lowchar) && currWord != "")
+                {
+                    if (dict.ContainsKey(currWord))
+                    {
+                        dict[currWord] += 1;
+                    }
+                    else
+                    {
+                        dict.Add(currWord, 1);
+                    }
+                    currWord = "";
+                }
+                if (IsLetter(lowchar))
+                {
+                    currWord += char.ToLower(lowchar);
+
+                }
+                if (i == paragraph.Count() - 1 && currWord != "")
+                {
+                    if (dict.ContainsKey(currWord))
+                    {
+                        dict[currWord] += 1;
+                    }
+                    else
+                    {
+                        dict.Add(currWord, 1);
+                    }
+                }
+            }
+
+            var max = 0;
+            var res = "";
+            foreach (var d in dict)
+            {
+                if (!banned.Contains(d.Key) && d.Value > max)
+                {
+                    res = d.Key;
+                    max = d.Value;
+                }
+            }
+            return res;
+        }
+
+
+
+
+        bool IsLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z');
+        }
+    }
+}
---
+++ TreePro/P145_BinaryTreePostorderTraversal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TreePro
+{
+    class P145_BinaryTreePostorderTraversal
+    {
+
+
+        public class TreeNode
+        {
+             public int val;
+             public TreeNode left;
+             public TreeNode right;
+             public TreeNode(int x) { val = x; }
+         }
+
+        // Recursive
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root == null) return result;
+            PostorderTraversalHelper(root, result);
+            return result;
+        }
+
+        void PostorderTraversalHelper(TreeNode root, List<int> list)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            PostorderTraversalHelper(root.left, list);
+            PostorderTraversalHelper(root.right, list);
+            list.Add(root.val);
+        }
+    }
+}
