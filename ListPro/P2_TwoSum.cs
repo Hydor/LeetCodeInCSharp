a517b8e11c220e58ea068563ce8ededdf49a6b34
Abu Hydor <bushiwo741@126.com>
2019-07-04 05:18:44 -0400

    note

--- ListPro/P2_TwoSum.cs
+++ ListPro/P2_TwoSum.cs
@@ -17,23 +17,23 @@ namespace LeetCode.ListPro
 
         public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            var result = new ListNode(0);
-            var head = result;
+            var result = new ListNode(0);    // new ListNode need an ininitial int
+            var head = result;    // ****** save the head for fanal return  ******
             var carry = 0;
             var n1 = 0;
             var n2 = 0;
             var sum = 0;
-            while (l1 != null || l2 != null || carry > 0)
+            while (l1 != null || l2 != null || carry > 0)     // carry > 0 
             {
                 result.next = new ListNode(0);
                 result = result.next;
-                n1 = l1 != null ? l1.val : 0;
+                n1 = l1 != null ? l1.val : 0;    
                 n2 = l2 != null ? l2.val : 0;
                 sum = n1 + n2 + carry;
                 result.val = sum % 10;
                 carry = sum / 10;
 
-                l1 = l1 != null ? l1.next : null;
+                l1 = l1 != null ? l1.next : null;   //  if not check , will cus NullReferenceException
                 l2 = l2 != null ? l2.next : null;
             }
             return head.next;
