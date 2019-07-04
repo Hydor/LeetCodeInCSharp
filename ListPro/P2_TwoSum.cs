9198376575a2a7b03a6912803bd65785cd3a1c63
Abu Hydor <bushiwo741@126.com>
2019-07-04 05:11:07 -0400

    p2

---
+++ ListPro/P2_TwoSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ListPro
+{
+    class P2_TwoSum
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var result = new ListNode(0);
+            var head = result;
+            var carry = 0;
+            var n1 = 0;
+            var n2 = 0;
+            var sum = 0;
+            while (l1 != null || l2 != null || carry > 0)
+            {
+                result.next = new ListNode(0);
+                result = result.next;
+                n1 = l1 != null ? l1.val : 0;
+                n2 = l2 != null ? l2.val : 0;
+                sum = n1 + n2 + carry;
+                result.val = sum % 10;
+                carry = sum / 10;
+
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+            }
+            return head.next;
+        }
+    }
+}
