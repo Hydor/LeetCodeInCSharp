7a2f290d3caf0fe9b36f824bbdad53b250625399
Abu Hydor <bushiwo741@126.com>
2019-07-07 01:02:02 -0400

    invertbt

---
+++ TreePro/P226_InvertBinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TreePro
+{
+    class P226_InvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            var left = InvertTree(root.left);
+            var right = InvertTree(root.right);
+            var temp = new TreeNode(0);
+            temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            return root;
+        }
+    }
+}
