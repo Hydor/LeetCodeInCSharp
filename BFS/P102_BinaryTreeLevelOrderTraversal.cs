2d41abb24e36965f0b24d63c528d729820c02629
Abu Hydor <bushiwo741@126.com>
2019-07-13 02:22:09 -0400

    bfs

---
+++ BFS/P102_BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BFS
+{
+    public class TreeNode
+    {
+         public int val;
+         public TreeNode left;
+         public TreeNode right;
+         public TreeNode(int x) { val = x; }
+    }
+
+    public class P102_BinaryTreeLevelOrderTraversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                var size = queue.Count();
+                var thislevel = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    var curr = queue.Dequeue();
+                    if (curr.left != null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+                    if (curr.right != null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }                     
+                    thislevel.Add(curr.val);
+                }
+                result.Add(thislevel);
+            }
+            return result;
+
+        }
+    }
+}
