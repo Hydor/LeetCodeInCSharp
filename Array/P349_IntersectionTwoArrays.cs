0817d086057bb56ef3cd1af986f9bf0f18589700
Abu Hydor <bushiwo741@126.com>
2019-09-02 03:57:23 -0400

    arr

---
+++ Array/P349_IntersectionTwoArrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Array
+{
+    class P349_IntersectionTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var res = new int[Math.Min(nums1.Length, nums2.Length)];
+            var index = 0;
+            if (nums1 == null || nums2 == null)
+            {
+                return res;
+            }
+            var dic = new Dictionary<int, int>();
+            foreach (var n1 in nums1)
+            {
+                if (!dic.ContainsKey(n1))
+                {
+                    dic.Add(n1, 1);
+                }
+            }
+            foreach (var n2 in nums2)
+            {
+                if (dic.ContainsKey(n2) && dic[n2] == 1)
+                {
+                    dic[n2] = 2;
+                    res[index++] = n2;
+                }
+            }
+            var realres = new int[index];
+            for (var i = 0; i < index; i++)
+            {
+                realres[i] = res[i];
+            }
+            return realres;
+        }
+    }
+}
---
+++ Array/P977_SquaresSortedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    class P977_SquaresSortedArray
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            var res = new int[A.Length];
+            if (A == null)
+            {
+                return res;
+            }
+            var index = A.Length - 1;
+            var leftPointer = 0;
+            var rightPointer = A.Length - 1;
+            var leftSquare = 0;
+            var rightSquare = 0;
+            while (leftPointer <= rightPointer)
+            {
+                leftSquare = A[leftPointer] * A[leftPointer];
+                rightSquare = A[rightPointer] * A[rightPointer];
+                if (leftSquare >= rightSquare)
+                {
+                    res[index--] = leftSquare;
+                    leftPointer++;
+                }
+                else
+                {
+                    res[index--] = rightSquare;
+                    rightPointer--;
+                }
+            }
+            return res;
+        }
+    }
+}
