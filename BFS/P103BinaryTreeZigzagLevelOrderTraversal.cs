e7c126c496d9821dfd29042e81ef55c612248a49
Abu Hydor <bushiwo741@126.com>
2019-09-07 04:00:02 -0400

    bfs

---
+++ BFS/P103BinaryTreeZigzagLevelOrderTraversal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BFS
+{
+    class P103BinaryTreeZigzagLevelOrderTraversal
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var resultList = new List<IList<int>>();
+            if (root == null)
+            {
+                return resultList;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var leftToRight = false;
+            while (queue.Any())
+            {
+                var size = queue.Count();
+                var currList = new List<int>();
+
+                for (var i = 0; i < size; i++)
+                {
+                    var currNode = queue.Dequeue();
+                    if (currNode.left != null)
+                    {
+                        queue.Enqueue(currNode.left);
+                    }
+                    if (currNode.right != null)
+                    {
+                        queue.Enqueue(currNode.right);
+                    }
+
+                    currList.Add(currNode.val);
+                }
+
+                leftToRight = !leftToRight;
+                if (!leftToRight)
+                {
+                    currList.Reverse();
+                }
+                resultList.Add(currList);
+
+            }
+            return resultList;
+        }
+    }
+}
---
+++ BFS/P107.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.BFS
+{
+    class P107
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Any())
+            {
+                var size = queue.Count();
+                var currList = new List<int>();
+
+                for (var i = 0; i < size; i++)
+                {
+                    var currNode = queue.Dequeue();
+                    if (currNode.left != null)
+                    {
+                        queue.Enqueue(currNode.left);
+                    }
+                    if (currNode.right != null)
+                    {
+                        queue.Enqueue(currNode.right);
+                    }
+                    currList.Add(currNode.val);
+                }
+                result.Add(currList);
+            }
+            result.Reverse();
+            return result;
+        }
+    }
+}
