fbd6ff12cabd1aa5697b7ac5e677556f615c4173
Abu Hydor <bushiwo741@126.com>
2019-09-06 22:42:24 -0400

    f

---
+++ P146_LRU_Cache.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LRUCache
+    {
+        
+            private int capacity;
+            public LRUCache(int capacity)
+            {
+            
+                this.capacity = capacity;
+            }
+
+            public int Get(int key)
+            {
+                if
+                return capacity.de
+            }
+
+            public void Put(int key, int value)
+            {
+
+            }
+        
+
+    }
+}
---
+++ TreePro/P111_MinimumDepthBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TreePro
+{
+    class P111_MinimumDepthBinaryTree
+    {
+        public int MinDepths;
+        public int MinDepth(TreeNode root)
+        {
+            MinDepths = int.MaxValue;
+            if (root == null)
+            {
+                return 0;
+            }
+            TraversalHelper(root, 1);
+            return MinDepths;
+        }
+        void TraversalHelper(TreeNode root, int depth)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            if (root.left == null && root.right == null)
+            {
+                MinDepths = Math.Min(MinDepths, depth);
+            }
+            TraversalHelper(root.left, depth + 1);
+            TraversalHelper(root.right, depth + 1);
+        }
+    }
+}
