aa68261967ea21b2fb5ac97a80d1246226b65d7a
Abu Hydor <bushiwo741@126.com>
2019-09-10 00:24:20 -0400

    guai

---
+++ BFS/P200_NumberofIslands.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BFS
+{
+    class P200_NumberofIslands
+    {
+        public class Point
+        {
+            public int row;
+            public int col;
+            public Point(int x, int y) { row = x; col = y; }
+        }
+        public static int NumIslands(char[][] grid)
+        {
+            var num = 0;
+            if (grid == null || grid.Length == 0)
+            {
+                return num;
+            }
+
+            int row = grid.Length;
+            int col = grid[0].Length;
+
+            var deltaX = new int[] { 0, 0, 1, -1 };
+            var deltaY = new int[] { 1, -1, 0, 0 };
+
+            for (var r = 0; r < row; r++)
+            {
+                for (var c = 0; c < col; c++)
+                {
+                    if (grid[r][c] == '1')
+                    {
+                        grid[r][c] = '0';
+                        num++;
+                        var queue = new Queue<Point>();
+                        var p = new Point(r, c);
+                        queue.Enqueue(p);
+                        while (queue.Any())
+                        {
+                            var currpoint = queue.Dequeue();
+                            for (var i = 0; i < 4; i++)
+                            {
+                                var nextrow = currpoint.row + deltaX[i];
+                                var nextcol = currpoint.col + deltaY[i];
+                                if (InBorder(row, col, nextrow, nextcol))
+                                {
+                                    if (grid[nextrow][nextcol] == '1')
+                                    {
+                                        grid[nextrow][nextcol] = '0';
+                                        var newP = new Point(nextrow, nextcol);
+                                        queue.Enqueue(newP);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            return num;
+        }
+
+        static bool InBorder(int row, int col, int currrow, int currcol)
+        {
+            return (row > currrow) && (col > currcol) && currcol >= 0 && currrow>= 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,19 +11,25 @@ namespace LeetCode
                 //var number1 = Convert.ToInt32(Console.ReadLine());
                 //var number2 = Convert.ToInt32(Console.ReadLine());
 
-                var a = new int[] {1, 2, 3, 4 ,5};
+                var a = new int[] { 1, 2, 3, 4, 5 };
 
                 var ab = new int[] { 7, 9, 1, 2, 6, 3, 4, 5 };
-                var arr2D = new int[,] { { 1, 4, 7, 11,15 }, { 2, 5, 8, 12,19 }, { 3, 6, 9, 16 ,22}, { 10, 13, 14, 17 ,24}, { 18, 21, 23, 26 ,31} };
-                var strarr = new string[] { "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do" };
-                var str = new string[] {"fasdfawx", "fefsfasdwf"};
+                var arr2D = new int[,] { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 }, { 18, 21, 23, 26, 31 } };
+               var strarr = new string[] {"11110", "11010", "11000", "00000"};
+                //var c = strarr[1].ToCharArray();
+                var chars = new char[4][];
+                for (var i = 0; i < 4; i++)
+                {
+                    chars[i] = strarr[i].ToCharArray();
+                }
+ 
                 Console.WriteLine(Convert.ToChar(97));
                 //var node = new TreePro.P101_SymmetricTree.TreeNode(1);
                 // node.left = new TreePro.P101_SymmetricTree.TreeNode(2);
                 // node.right = new TreePro.P101_SymmetricTree.TreeNode(2);
                 //  node.left.left = new TreePro.P101_SymmetricTree.TreeNode(3);
 
-                Console.WriteLine(TopoSorting.P269_AlienDictionary.AlienOrder(str));
+                Console.WriteLine(BFS.P200_NumberofIslands.NumIslands(chars));
 
                 //foreach (var i in ab)
                 //{
@@ -32,6 +38,7 @@ namespace LeetCode
               
                 var str2 = Console.ReadLine();
             }
+
         }
 
 
