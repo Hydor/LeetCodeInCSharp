a0d1426aeef625e79e1ab1a6694a943115e8d99d
Abu Hydor <bushiwo741@126.com>
2019-09-16 03:04:27 -0400

    reverseList

---
+++ ListPro/P206ReverseLinkedList.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace LeetCode.ListPro
+{
+    class P206ReverseLinkedList
+    {
+
+        public class ListNode
+        {
+             public int val;
+             public ListNode next;
+             public ListNode(int x) { val = x; }
+         }
+
+
+        // Approach 1 : Recursive
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+            ListNode p = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return p;
+        }
+
+        // Approach 2 : Iterative
+        public ListNode ReverseList2(ListNode head)
+        {
+            ListNode pre = null;
+            var curr = head;
+            while (curr.next != null)
+            {
+                var temp = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = temp;
+            }
+            return pre;
+        }
+
+
+    }
+}
